c636b773c91b0ebf75bb9ef4c3b2856edabd1ae0
zach lieberman <zach@eyebeam.org>
2014-11-11 11:00:48 -0500

    added uniq to bible example

---
+++ sortBible_set.py
@@ -0,0 +1,49 @@
+
+
+# print "hello"
+# words = ['hello','mom','how','are','you']
+# print len(words)
+# words = sorted(words)
+# for word in words:
+# 	print word
+
+with open ("bible-kjv.txt", "r") as myfile:
+	data=myfile.read()
+
+words = data.split()
+
+cleanWords = []
+
+for word in words:
+	word = word.strip()
+	word = word.lower()
+	cleanWords.append(word)
+
+
+punctuations = '''0123456789!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+superCleanWords = []
+
+for word in cleanWords:
+	noPunct = ""
+	for char in word:
+		if char not in punctuations:
+			noPunct = noPunct + char
+	if noPunct is not "":
+		superCleanWords.append(noPunct)
+
+superCleanWords = set(superCleanWords)
+superCleanWords = sorted(superCleanWords)
+
+# for word in superCleanWords:
+# 	print word
+
+
+outputFile = open("biblesortedUniq.txt", 'w')
+
+for word in superCleanWords:
+	outputFile.write(word + " ")
+
+outputFile.close()
+
+
